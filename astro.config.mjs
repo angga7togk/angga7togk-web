eea128e3e940dbfa4fb9a2e3dd0c8cd0ae144de5
Angga7Togk <kiplihode123321@gmail.com>
2025-01-14 21:27:03 +0700

    node

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,16 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 
+import node from "@astrojs/node";
+
 // https://astro.build/config
 export default defineConfig({
   server: {
     host: "0.0.0.0",
     port: 8881,
   },
+
+  adapter: node({
+    mode: "standalone",
+  }),
 });
